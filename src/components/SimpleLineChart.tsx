8362962676c18a9997b8dbdddf277aeddaed7e45
Uhmopt <qethum@gmail.com>
2023-01-14 05:35:31 +0900

    [Update] chart

--- src/components/SimpleLineChart.tsx
+++ src/components/SimpleLineChart.tsx
@@ -89,7 +89,6 @@ const ChartItem: FC<
         height: BAR_HEIGHT,
         backgroundColor: color,
         color: '#fff',
-        transform: `translateX(${(min / distance) * 100}%)`,
       }}
     >
       <span style={{ padding: GAP }}>{toNumber(value).toFixed(2)}</span>
