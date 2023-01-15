f3dd954787dd6f734f7db86aef2bfd52d23e389e
Uhmopt <qethum@gmail.com>
2023-01-15 14:56:24 +0900

    [new] axis

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -49,37 +49,18 @@ export function LineChart({
           },
         },
       },
-      // ...data.slice(0, 2).reduce(
-      //   (ret, axis, axisIndex) => ({
-      //     ...ret,
-      //     [`y${axisIndex + 1}`]: {
-      //       position: axisIndex === 0 ? ('left' as const) : ('right' as const),
-      //       grid: {
-      //         drawBorder: false,
-      //       },
-      //     },
-      //   }),
-      //   {}
-      // ),
-      y: {
-        position: 'left' as const,
-        ticks: {
-          stepSize: 1,
-          beginAtZero: true,
-        },
-        grid: {
-          drawBorder: false,
-        },
-      },
-      y1: {
-        position: 'right' as const,
-        ticks: {
-          beginAtZero: true,
-        },
-        grid: {
-          drawBorder: false,
-        },
-      },
+      ...data.slice(0, 2).reduce(
+        (ret, axis, axisIndex) => ({
+          ...ret,
+          [axisIndex ? `y${axisIndex + 1}` : 'y']: {
+            position: axisIndex === 0 ? ('left' as const) : ('right' as const),
+            grid: {
+              drawBorder: false,
+            },
+          },
+        }),
+        {}
+      ),
     },
   };
 
