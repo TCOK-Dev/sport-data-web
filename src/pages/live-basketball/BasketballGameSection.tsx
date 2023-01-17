202c182930fd6d58bfa2048fedad6efb0cc45f7e
Uhmopt <qethum@gmail.com>
2023-01-18 02:14:01 +0900

    [New] component structure

---
+++ src/pages/live-basketball/BasketballGameSection.tsx
@@ -0,0 +1,39 @@
+import { Typography } from '@mui/material';
+import { FC, PropsWithChildren, useMemo } from 'react';
+import { BasketballGame } from '../../types/basketball-game.types';
+import BasketballGameList from './BasketballGameList';
+
+const TOP_3_TIME_LIMIT = 300;
+
+const BasketballGameSection: FC<
+  PropsWithChildren<{ data?: Array<BasketballGame>; title?: string }>
+> = ({ data = [], title = 'Games' }) => {
+  const top3Games: Array<BasketballGame> = useMemo(() => {
+    return data
+      .filter((item) => item.playedTime > TOP_3_TIME_LIMIT)
+      .sort((a, b) =>
+        Math.abs(a.awayScore - a.homeScore) >
+        Math.abs(b.awayScore - b.homeScore)
+          ? -1
+          : 1
+      )
+      .slice(0, 3);
+  }, [data]);
+
+  return (
+    <div>
+      <Typography variant='h5' color='primary' fontWeight={500}>
+        Top 3
+      </Typography>
+      <BasketballGameList data={data} />
+      <br />
+
+      <Typography variant='h5' color='primary' fontWeight={500}>
+        {title}
+      </Typography>
+      <BasketballGameList data={top3Games} />
+    </div>
+  );
+};
+
+export default BasketballGameSection;
--- src/pages/live-basketball/LiveBasketball.tsx
+++ src/pages/live-basketball/LiveBasketball.tsx
@@ -1,13 +1,10 @@
-import { Typography } from '@mui/material';
 import { FC, PropsWithChildren, useEffect, useMemo, useState } from 'react';
 import TabsContainer from '../../components/TabsContainer';
 import { REFRESH_TIME_INTERVAL } from '../../constants/global';
 import { APIResponseCode } from '../../services/apiResponse';
 import basketballGameService from '../../services/basketball-game.services';
 import { BasketballGame } from '../../types/basketball-game.types';
-import BasketballGameList from './BasketballGameList';
-
-const TOP_3_TIME_LIMIT = 300;
+import BasketballGameSection from './BasketballGameSection';
 
 const LiveBasketball: FC<PropsWithChildren<{}>> = () => {
   const [data, setData] = useState<Array<BasketballGame>>([]);
@@ -16,33 +13,9 @@ const LiveBasketball: FC<PropsWithChildren<{}>> = () => {
     return data.filter((game) => game.quarter?.[1] === 'Q');
   }, [data]);
 
-  const nbaTop3Games: Array<BasketballGame> = useMemo(() => {
-    return nbaGames
-      .filter((item) => item.playedTime > TOP_3_TIME_LIMIT)
-      .sort((a, b) =>
-        Math.abs(a.awayScore - a.homeScore) >
-        Math.abs(b.awayScore - b.homeScore)
-          ? -1
-          : 1
-      )
-      .slice(0, 3);
-  }, [nbaGames]);
-
   const cgGames: Array<BasketballGame> = useMemo(() => {
     return data.filter((game) => game.quarter?.[1] === 'H');
   }, [data]);
-
-  const cgTop3Games: Array<BasketballGame> = useMemo(() => {
-    return cgGames
-      .filter((item) => item.playedTime > TOP_3_TIME_LIMIT)
-      .sort((a, b) =>
-        Math.abs(a.awayScore - a.homeScore) >
-        Math.abs(b.awayScore - b.homeScore)
-          ? -1
-          : 1
-      )
-      .slice(0, 3);
-  }, [cgGames]);
 
   const loadData = async () => {
     const res = await basketballGameService.getsLive();
@@ -65,37 +38,11 @@ const LiveBasketball: FC<PropsWithChildren<{}>> = () => {
         data={[
           {
             label: 'NBA',
-            node: (
-              <div>
-                <Typography variant='h5' color='primary' fontWeight={500}>
-                  Top 3
-                </Typography>
-                <BasketballGameList data={nbaTop3Games} />
-                <br />
-
-                <Typography variant='h5' color='primary' fontWeight={500}>
-                  NBA
-                </Typography>
-                <BasketballGameList data={nbaGames} />
-              </div>
-            ),
+            node: <BasketballGameSection title='NBA' data={nbaGames} />,
           },
           {
             label: 'College',
-            node: (
-              <div>
-                <Typography variant='h5' color='primary' fontWeight={500}>
-                  Top 3
-                </Typography>
-                <BasketballGameList data={cgTop3Games} />
-                <br />
-
-                <Typography variant='h5' color='primary' fontWeight={500}>
-                  College
-                </Typography>
-                <BasketballGameList data={cgGames} />
-              </div>
-            ),
+            node: <BasketballGameSection title='College' data={cgGames} />,
           },
         ]}
       />
